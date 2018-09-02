4949c1483958842b09f8f3daf478d842e3abd21e
Blaž Pečnik <blaz@easistent.com>
2018-09-02 15:44:55 +0200

    feat: server

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,8 @@
 import express from "express";
 
+const PORT = 8080;
 const app = express();
-const srv = app.listen(8080);
-app.use("/", express.static(__dirname + "/../dist/"));
+const srv = app.listen(PORT);
+app.use("/", express.static(__dirname + "/../../dist"));
+
+console.log("Server running at http://localhost:" + PORT);
