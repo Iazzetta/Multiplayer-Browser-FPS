3a5727a5d91103bc53174af860b0effcc851ccdb
Blaž Pečnik <blaz@easistent.com>
2018-09-02 15:50:50 +0200

    feat: socket connection

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,18 @@
 import express from "express";
+import SocketIO from "socket.io";
 
 const PORT = 8080;
 const app = express();
 const srv = app.listen(PORT);
 app.use("/", express.static(__dirname + "/../../dist"));
+
+const io = SocketIO.listen(srv);
+io.sockets.on("connection", socket => {
+    console.log("connection", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("disconnect", socket.id);
+    });
+});
 
 console.log("Server running at http://localhost:" + PORT);
