6e331ceb1f719c56dcdfd55fcf9d8f458cb0ddc6
Blaž Pečnik <blaz@easistent.com>
2018-09-08 09:17:29 +0200

    feat: added dummy player to single-player mode

--- src/client/client.js
+++ src/client/client.js
@@ -3,7 +3,7 @@ import { initGame, setScreenSize } from "../game/actions.js";
 
 const game = new Game();
 
-game.dispatch(initGame([game.playerId()]));
+game.dispatch(initGame([game.playerId(), "dummy-player"]));
 game.dispatch(setScreenSize(window.innerWidth, window.innerHeight));
 
 requestAnimationFrame(function next() {
