43fbf2c7ad8d4d2e1c51e6d1823f11df9db425dd
Blaž Pečnik <blaz@easistent.com>
2018-09-27 18:00:22 +0200

    refactro: NEW ACTION LIST

--- src/game/actions.js
+++ src/game/actions.js
@@ -1,106 +1,45 @@
-import { State } from "./state";
+import times from "lodash/times";
+
+export const [
+    // Special actions
+    EMPTY,
+
+    // Sync local actions
+    SERVER_ACTION,
+    CLIENT_ACTION,
+
+    // Sync players
+    PLAYER_JOIN,
+    PLAYER_LEAVE,
+    SYNC_PLAYER,
+    SYNC_PLAYER_SCORE,
+    SYNC_ALL_PLAYERS,
+
+    // Spawn
+    SPAWN_PLAYER,
+    SPAWN_BULLET_PACK,
+    SPAWN_HEALTH_PACK,
+
+    // Player inputs
+    SET_CAMERA_VIEW,
+    SET_INPUT,
+    SET_AIM,
+
+    // Gameplay actions
+    SHOOT_BULLET,
+    RELOAD_START,
+    RELOAD_DONE,
+    HIT_PLAYER,
+    KILL_PLAYER
+] = times(100);
 
 export class Action {
     /**
-     * @param {string} type
+     * @param {number} type
      * @param {object} data
      */
     constructor(type, data) {
         this.type = type;
         this.data = data;
     }
-}
-
-/**
- * @param {Action} action
- */
-export function serverAction(action) {
-    return new Action("SERVER_ACTION", action);
-}
-
-/**
- * @param {string[]} playerIds
- */
-export function initGame(playerIds) {
-    return new Action("INIT_GAME", { playerIds });
-}
-
-/**
- * @param {number} width
- * @param {number} height
- */
-export function setScreenSize(width, height) {
-    return new Action("SET_SCREEN_SIZE", { width, height });
-}
-
-/**
- * @param {string} playerId
- * @param {string} input
- * @param {boolean} value
- */
-export function setPlayerInput(playerId, input, value) {
-    return new Action("SET_PLAYER_INPUT", { playerId, input, value });
-}
-
-/**
- * @param {string} playerId
- * @param {number} ver
- * @param {number} hor
- */
-export function setPlayerAim(playerId, ver, hor) {
-    return new Action("SET_PLAYER_AIM", { playerId, ver, hor });
-}
-
-/**
- * @param {string} playerId
- */
-export function shootBullet(playerId) {
-    return new Action("SHOOT_BULLET", { playerId });
-}
-
-/**
- * @param {string} playerId
- * @param {number} hp
- */
-export function setHP(playerId, hp) {
-    return new Action("SET_HP", { playerId, hp });
-}
-
-/**
- * @param {string} id
- */
-export function kill(id) {
-    return new Action("KILL", { id });
-}
-
-/**
- * @param {string[]} playerIds
- */
-export function syncGameState(playerIds) {
-    return new Action("SYNC_GAME_STATE", { playerIds });
-}
-
-/**
- * @param {string} playerId
- * @param {State} state
- */
-export function syncPlayer(playerId, state) {
-    const { head, object3D, velocity } = state.getEntityComponents(playerId);
-    if (object3D == undefined) return;
-
-    const { x, y, z } = object3D.position;
-    const { x: vx, y: vy, z: vz } = velocity;
-    const rx = head.rotation.x;
-    const ry = object3D.rotation.y;
-    return new Action("SYNC_PLAYER", {
-        playerId,
-        x,
-        y,
-        z,
-        vx,
-        vy,
-        vz,
-        rx,
-        ry
-    });
 }
--- src/game/dispatch.js
+++ src/game/dispatch.js
@@ -1,6 +1,4 @@
 import * as THREE from "three";
-import { TILE_SIZE } from "./consts.js";
-import { Action } from "./actions.js";
 import { State } from "./state.js";
 import {
     Entity,
@@ -11,7 +9,25 @@ import {
     JetpackPickup,
     HpPickup
 } from "./entities";
-import { forEachMapTile } from "./utils.js";
+import {
+    Action,
+    PLAYER_JOIN,
+    PLAYER_LEAVE,
+    SYNC_PLAYER,
+    SYNC_PLAYER_SCORE,
+    SYNC_ALL_PLAYERS,
+    SPAWN_PLAYER,
+    SPAWN_BULLET_PACK,
+    SPAWN_HEALTH_PACK,
+    SET_CAMERA_VIEW,
+    SET_INPUT,
+    SET_AIM,
+    SHOOT_BULLET,
+    RELOAD_START,
+    RELOAD_DONE,
+    HIT_PLAYER,
+    KILL_PLAYER
+} from "./actions.js";
 
 /**
  * @param {State} state
@@ -19,205 +35,72 @@ import { forEachMapTile } from "./utils.js";
  */
 export function dispatch(state, action) {
     switch (action.type) {
-        case "SET_HP": {
-            const { playerId, hp } = action.data;
-            const { health } = state.getEntityComponents(playerId);
-            if (health !== undefined) {
-                health.hp = hp;
-            }
-            return state;
-        }
-        case "KILL": {
+        case PLAYER_JOIN: {
             const { id } = action.data;
-            state.deleteEntity(id);
-            return state;
-        }
-        case "SYNC_PLAYER": {
-            const { playerId, x, y, z, vx, vy, vz, rx, ry } = action.data;
-            const player = state.getEntity(playerId);
-            if (player !== undefined) {
-                if (player.head !== undefined) {
-                    player.head.rotation.x = rx;
-                }
-
-                if (player.object3D !== undefined) {
-                    player.object3D.position.set(x, y, z);
-                    player.object3D.rotation.y = ry;
-                }
-
-                if (player.velocity !== undefined) {
-                    player.velocity.set(vx, vy, vz);
-                }
-            }
-            return state;
-        }
-        case "SET_SCREEN_SIZE": {
-            const { width, height } = action.data;
-            state.camera.aspect = width / height;
-            state.camera.updateProjectionMatrix();
-            return state;
-        }
-        case "SET_PLAYER_INPUT": {
-            const { playerId, input, value } = action.data;
-            const { controller } = state.getEntityComponents(playerId);
-            if (controller !== undefined) {
-                if (controller.input[input] !== undefined) {
-                    controller.input[input] = value;
-                }
-            }
-            return state;
-        }
-        case "SET_PLAYER_AIM": {
-            const { playerId, ver, hor } = action.data;
-            const { object3D, head } = state.getEntityComponents(playerId);
-            if (object3D && head) {
-                object3D.rotation.y = ver;
-                head.rotation.x = hor;
-            }
-            return state;
-        }
-        case "SHOOT_BULLET": {
-            const { playerId } = action.data;
-            const player = state.getEntity(playerId);
-            if (player && player.object3D && player.head) {
-                // Create bullet
-                const bulletId = playerId + Date.now().toString(16);
-                const bullet = new Bullet(bulletId, state.assets);
-                bullet.damage.creatorId = player.id;
-
-                // Set velocity
-                const bulletSpeed = 0.05;
-                const direction = player.head.getFacingDirection();
-                bullet.velocity.z = direction.z * bulletSpeed;
-                bullet.velocity.x = direction.x * bulletSpeed;
-                bullet.velocity.y = direction.y * bulletSpeed;
-
-                // Set position
-                const playerAABB = player.object3D.getAABB();
-                bullet.object3D.position.x = player.object3D.position.x;
-                bullet.object3D.position.y = playerAABB.max.y - 0.5;
-                bullet.object3D.position.z = player.object3D.position.z;
-
-                // Offset infrotn of camera
-                const DIST = 1.25;
-                const offset = new THREE.Vector3();
-                offset.copy(bullet.velocity);
-                offset.normalize();
-                offset.multiply(new THREE.Vector3(DIST, DIST, DIST));
-                bullet.object3D.position.add(offset);
-
-                state.addEntity(bullet);
-            }
-            return state;
-        }
-        case "INIT_GAME": {
-            const { playerIds } = action.data;
-
-            state = new State(state.assets);
-            state.time.start = Date.now();
-            state.playerIds = playerIds;
-            state.playerSpawns = [];
-
-            forEachMapTile((id, x, y, z) => {
-                const entity = createEntity(id, state);
-                const vector = new THREE.Vector3(
-                    TILE_SIZE * x,
-                    TILE_SIZE * y,
-                    TILE_SIZE * z
-                );
-
-                if (entity !== undefined) {
-                    if (entity.object3D) {
-                        entity.object3D.position.copy(vector);
-                    }
-                    state.addEntity(entity);
-                }
-
-                // Save player spawn
-                if (id === 1) {
-                    state.playerSpawns.push(vector.clone());
-                }
-            });
-
-            // Create lights ...
-            const dirLight = (color, int) => {
-                return new THREE.DirectionalLight(new THREE.Color(color), int);
-            };
-
-            var light = new THREE.AmbientLight(0x404040);
-            state.scene.add(light);
-
-            const keyLight = dirLight("#FFE4C4", 0.74);
-            keyLight.position.set(-100, 50, 100);
-            state.scene.add(keyLight);
-
-            const fillLight = dirLight("#A6D8ED", 0.25);
-            fillLight.position.set(100, 50, 100);
-            state.scene.add(fillLight);
-
-            const backLight = dirLight("#FFFFFF", 0.5);
-            backLight.position.set(100, 0, -100).normalize();
-            state.scene.add(backLight);
-
-            return state;
-        }
-        case "SYNC_GAME_STATE": {
-            const { playerIds } = action.data;
-            const spawns = state.playerSpawns;
-
-            // Add player
-            playerIds.forEach((id, index) => {
-                const player = state.getEntity(id);
-                if (player === undefined) {
-                    const player = new Player(id, state.assets);
-                    const spawn = spawns[index % spawns.length];
-
-                    player.object3D.position.copy(spawn);
-                    state.addEntity(player);
-                }
-            });
-
-            // Remove missing players
             state.playerIds
-                .filter(id => playerIds.indexOf(id) === -1)
-                .forEach(id => state.deleteEntity(id));
-
-            state.playerIds = playerIds;
-
             return state;
         }
-    }
-    return state;
-}
+        case PLAYER_LEAVE: {
+            const {} = action.data;
+            return state;
+        }
+        case SYNC_PLAYER: {
+            const {} = action.data;
+            return state;
+        }
+        case SYNC_PLAYER_SCORE: {
+            const {} = action.data;
+            return state;
+        }
+        case SYNC_ALL_PLAYERS: {
+            const {} = action.data;
+            return state;
+        }
 
-/**
- * @param {number} tileId
- * @param {State} state
- * @return {Entity}
- */
-export function createEntity(tileId, state) {
-    const entityId = (128 + state.entities.size).toString(16);
-    const assets = state.assets;
-    switch (tileId) {
-        case 1: {
-            const index = state.getEntityGroup("player").length;
-            const playerId = state.playerIds[index];
-            if (playerId !== undefined) {
-                return new Player(playerId, assets);
-            }
-            return;
+        case SPAWN_PLAYER: {
+            const {} = action.data;
+            return state;
         }
-        case 2: {
-            return new Wall(entityId, assets);
+        case SPAWN_BULLET_PACK: {
+            const {} = action.data;
+            return state;
         }
-        case 3: {
-            return new AmmoPickup(entityId, assets);
+        case SPAWN_HEALTH_PACK: {
+            const {} = action.data;
+            return state;
         }
-        case 4: {
-            return new JetpackPickup(entityId, assets);
+        case SET_CAMERA_VIEW: {
+            const {} = action.data;
+            return state;
         }
-        case 5: {
-            return new HpPickup(entityId, assets);
+        case SET_INPUT: {
+            const {} = action.data;
+            return state;
         }
+        case SET_AIM: {
+            const {} = action.data;
+            return state;
+        }
+        case SHOOT_BULLET: {
+            const {} = action.data;
+            return state;
+        }
+        case RELOAD_START: {
+            const {} = action.data;
+            return state;
+        }
+        case RELOAD_DONE: {
+            const {} = action.data;
+            return state;
+        }
+        case HIT_PLAYER: {
+            const {} = action.data;
+            return state;
+        }
+        case KILL_PLAYER: {
+            return state;
+        }
+        default:
+            return state;
     }
 }
