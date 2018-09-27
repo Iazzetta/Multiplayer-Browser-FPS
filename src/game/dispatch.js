227c4e8416b2f7e232460cd16c93c5a12b027fdf
Blaž Pečnik <blaz@easistent.com>
2018-09-27 18:05:52 +0200

    feat: spawn players

--- src/game/dispatch.js
+++ src/game/dispatch.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import uniq from "lodash/uniq";
+import without from "lodahs/without";
 import { State } from "./state.js";
 import {
     Entity,
@@ -37,15 +39,31 @@ export function dispatch(state, action) {
     switch (action.type) {
         case PLAYER_JOIN: {
             const { id } = action.data;
-            state.playerIds
+            state.playerIds = uniq(state.playerIds.concat([id]));
             return state;
         }
         case PLAYER_LEAVE: {
-            const {} = action.data;
+            const { id } = action.data;
+            state.playerIds = without(state.playerIds, [id]);
             return state;
         }
         case SYNC_PLAYER: {
-            const {} = action.data;
+            const { id, x, y, z, vx, vy, vz, rx, ry } = action.data;
+            const player = state.getEntity(id);
+            if (player !== undefined) {
+                if (player.head !== undefined) {
+                    player.head.rotation.x = rx;
+                }
+
+                if (player.object3D !== undefined) {
+                    player.object3D.position.set(x, y, z);
+                    player.object3D.rotation.y = ry;
+                }
+
+                if (player.velocity !== undefined) {
+                    player.velocity.set(vx, vy, vz);
+                }
+            }
             return state;
         }
         case SYNC_PLAYER_SCORE: {
@@ -56,7 +74,6 @@ export function dispatch(state, action) {
             const {} = action.data;
             return state;
         }
-
         case SPAWN_PLAYER: {
             const {} = action.data;
             return state;
