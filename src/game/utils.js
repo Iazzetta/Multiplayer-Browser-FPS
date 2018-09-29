cc1537bf5dc9017e8dcde95ef2ab4e4b8f9b334c
Blaž Pečnik <blaz@easistent.com>
2018-09-29 19:24:39 +0200

    refactor: removed old level assets

--- src/game/utils.js
+++ src/game/utils.js
@@ -1,8 +1,5 @@
 import * as THREE from "three";
 
-// @ts-ignore
-import cube_land_map from "../../dist/assets/maps/small-room.json";
-
 export class AABB {
     constructor(min = new THREE.Vector3(), max = new THREE.Vector3()) {
         this.min = min;
@@ -60,29 +57,4 @@ export function toRadians(degrees) {
  */
 export function toDegrees(radians) {
     return (radians * 180) / Math.PI;
-}
-
-/**
- * @param {(id:number,x:number,y:number,z:number) => any} f
- */
-export function forEachMapTile(f) {
-    const level = cube_land_map;
-    const size = new THREE.Vector3(
-        cube_land_map.width,
-        cube_land_map.layers.length,
-        cube_land_map.height
-    );
-
-    for (let y = 0; y < size.y; y++) {
-        const layer = level.layers[y];
-        for (let x = 0; x < size.x; x++) {
-            for (let z = 0; z < size.z; z++) {
-                const index = z * size.z + x;
-                const id = layer.data[index];
-                if (id > 0) {
-                    f(id, x, y, z);
-                }
-            }
-        }
-    }
 }
