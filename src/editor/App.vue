fc57fd3b4f4e3360de980780827801962b55b6d1
Blaž Pečnik <blaz@easistent.com>
2018-10-11 12:31:15 +0200

    new editor - removed old code

---
+++ src/editor/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="app">
+        APP
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("App mounted");
+    }
+};
+</script>
+
+<style lang="scss">
+html,
+body {
+    margin: 0;
+    padding: 0;
+}
+</style>
---
+++ src/editor/index.js
@@ -0,0 +1,8 @@
+import Vue from "vue/dist/vue.esm.js";
+
+// @ts-ignore
+import App from "./App.vue";
+
+new Vue({
+    components: { App }
+}).$mount("#app");
