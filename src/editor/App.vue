f13615a2cd187167e470e6062267a7cf0683ea49
Blaž Pečnik <blaz@easistent.com>
2018-10-11 13:44:21 +0200

    feat: level-editor - world

--- src/editor/App.vue
+++ src/editor/App.vue
@@ -19,13 +19,15 @@ import Menu from "./components/Menu.vue";
 export default {
     components: {
         Screen,
-        Menu,
+        Menu
     }
 };
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 .app {
+    width: 100vw;
+    height: 100vh;
     display: grid;
     grid-gap: 4px;
     grid-template-rows: 64px 1fr 64px;
@@ -34,26 +36,24 @@ export default {
     .header {
         grid-row: 1/2;
         grid-column: 2/10;
-
         line-height: 64px;
     }
 
     .footer {
         grid-row: 3/4;
         grid-column: 2/10;
-
-        line-height: 64px;
-        text-align: center;
     }
 
     .menu {
         grid-row: 2/3;
         grid-column: 2/4;
+        position: relative;
     }
 
     .screen {
         grid-row: 2/3;
         grid-column: 4/10;
+        position: relative;
     }
 }
 </style>
--- src/editor/components/Screen.vue
+++ src/editor/components/Screen.vue
@@ -1,17 +1,55 @@
 <template>
-    <div class="screen"></div>
+    <div class="world-container" ref="screen">
+        <div class="world" ref="world">
+            <div class="axis-x"></div>
+            <div class="axis-y"></div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {};
+export default {
+    mounted() {
+        this.$nextTick(() => {
+            const { screen, world } = this.$refs;
+            screen.scrollLeft = world.scrollWidth - screen.clientWidth;
+            screen.scrollTop = world.scrollHeight - screen.clientHeight;
+
+            screen.scrollLeft = screen.scrollLeft * 0.5;
+            screen.scrollTop = screen.scrollTop * 0.5;
+        });
+    }
+};
 </script>
 
 <style scoped lang="scss">
-.screen {
-    position: relative;
+.world-container {
+    position: absolute;
     width: 100%;
     height: 100%;
-
+    overflow: auto;
     background-color: black;
+
+    .world {
+        position: relative;
+        width: 2048px;
+        height: 2048px;
+
+        * {
+            position: absolute;
+        }
+
+        .axis-x {
+            top: 50%;
+            width: 100%;
+            border-top: 1px solid red;
+        }
+
+        .axis-y {
+            left: 50%;
+            height: 100%;
+            border-left: 1px solid limegreen;
+        }
+    }
 }
 </style>
