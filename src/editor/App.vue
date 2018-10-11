8a974c1446f3fdb2cfba50d80fc8a2f530d43008
Blaž Pečnik <blaz@easistent.com>
2018-10-11 12:52:07 +0200

    feat: level editor layout

--- src/editor/App.vue
+++ src/editor/App.vue
@@ -1,21 +1,48 @@
 <template>
     <div class="app">
-        APP
+        <div class="header">
+            Level editor
+        </div>
+        <div class="menu"></div>
+        <div class="screen"></div>
+        <div class="footer"></div>
     </div>
 </template>
 
 <script>
-export default {
-    mounted() {
-        console.log("App mounted");
-    }
-};
+export default {};
 </script>
 
 <style lang="scss">
-html,
-body {
-    margin: 0;
-    padding: 0;
+.app {
+    display: grid;
+    grid-gap: 4px;
+    grid-template-rows: 64px 1fr 64px;
+    grid-template-columns: 8px repeat(8, 1fr) 8px;
+
+    .header {
+        grid-row: 1/2;
+        grid-column: 2/10;
+
+        line-height: 64px;
+    }
+
+    .footer {
+        grid-row: 3/4;
+        grid-column: 2/10;
+
+        line-height: 64px;
+        text-align: center;
+    }
+
+    .menu {
+        grid-row: 2/3;
+        grid-column: 2/4;
+    }
+
+    .screen {
+        grid-row: 2/3;
+        grid-column: 4/10;
+    }
 }
 </style>
