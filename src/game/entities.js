6b5cb65b94100ad398de3656aa807d9145a6a944
Blaž Pečnik <blaz@easistent.com>
2018-10-25 11:32:31 +0200

    fix: removed hoverboard model

--- src/game/entities.js
+++ src/game/entities.js
@@ -155,10 +155,6 @@ export class PlayerEntity extends Entity {
         body.castShadow = true;
         this.playerModel.bodyModel.add(body);
 
-        const board = assets.mesh("player_hover_board");
-        board.castShadow = false;
-        this.playerModel.bodyModel.add(board);
-
         this.playerModel.setMode("third-person");
     }
 }
