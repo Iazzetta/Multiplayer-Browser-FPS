27969788a2f752537c057f1f00edc127ad5a6d56
Blaž Pečnik <blaz@easistent.com>
2018-10-25 19:15:37 +0200

    feat: place and move tiles

--- src/editor-3d/index.js
+++ src/editor-3d/index.js
@@ -2,20 +2,92 @@ import * as THREE from "three";
 import { Game } from "../client/js/game";
 import { playerJoin, setMyPlayerId, spawnTileEntity } from "../game/actions";
 import { hitScan } from "../game/utils";
+import times from "lodash/times";
+import { TileEntity } from "../game/entities";
 
 class Editor extends Game {
     constructor() {
         super();
         this.activeTileId = Date.now().toString(16);
+        this.tileIndex = 0;
+        this.tiles = [
+            "tile_box-sm",
+            "tile_box-md",
+            "tile_box-lg",
+            "tile_wall-sm",
+            "tile_wall-md",
+            "tile_wall-lg",
+            "tile_floor-sm",
+            "tile_floor-lg",
+            "tile_pillar-sm",
+            "tile_pillar-md"
+        ];
     }
 
-    onMouseDown() {
-        this.activeTileId = Date.now().toString(16);
-        this.dispatch(spawnTileEntity(this.activeTileId, "tile_box-sm"));
+    /**
+     *
+     * @param {KeyboardEvent} ev
+     */
+    onKeyDown(ev) {
+        super.onKeyDown(ev);
+
+        const [TAB, DEL] = [9, 46];
+
+        if (ev.keyCode === TAB) {
+            ev.preventDefault();
+            ev.stopPropagation();
+
+            if (this.activeTileId === null) {
+                this.activeTileId = Date.now().toString(16);
+            }
+
+            this.tileIndex = this.tileIndex + 1;
+            this.tileIndex = this.tileIndex % this.tiles.length;
+
+            const id = this.activeTileId;
+            const mesh = this.tiles[this.tileIndex];
+            this.dispatch(spawnTileEntity(id, mesh));
+        }
+
+        if (ev.keyCode === DEL) {
+            if (this.activeTileId) {
+                this.state.deleteEntity(this.activeTileId);
+                this.activeTileId = null;
+            }
+        }
+    }
+
+    /**
+     * @param {MouseEvent} ev
+     */
+    onMouseDown(ev) {
+        if (ev.button === 2) {
+            // Select
+            const { playerModel } = this.myComponents;
+            const hitscan = hitScan(
+                playerModel.camera,
+                this.state,
+                this.activeTileId
+            );
+
+            if (hitscan.entity instanceof TileEntity) {
+                if (hitscan.entity.id !== "flor") {
+                    this.activeTileId = hitscan.entity.id;
+                }
+            }
+        } else {
+            // Place tile
+            this.activeTileId = null;
+            this.tileIndex = 0;
+        }
     }
 
     onMouseMove(ev) {
         super.onMouseMove(ev);
+    }
+
+    update() {
+        super.update();
 
         const { playerModel } = this.myComponents;
         const hitscan = hitScan(
@@ -37,9 +109,12 @@ class Editor extends Game {
                 const direction = new THREE.Vector3();
                 const aabb = hitscan.entity.object3D.toAABB();
 
-                const getdirection = axis =>
-                    (aabb.max[axis] === entry[axis] ? 1 : 0) +
-                    (aabb.min[axis] === entry[axis] ? -1 : 0);
+                const getdirection = axis => {
+                    const p = Math.round(entry[axis] * 1000);
+                    const max = Math.round(aabb.max[axis] * 1000);
+                    const min = Math.round(aabb.min[axis] * 1000);
+                    return (max === p ? 1 : 0) + (min === p ? -1 : 0);
+                };
 
                 direction.x = getdirection("x");
                 direction.y = getdirection("y");
